b65ab0a776541f6392a6779cacc60492e27191df
YOGASUBHAKARABABU <subhakarababu@gmail.com>
2017-12-20 14:27:10 +0530

    first commit

---
+++ app/src/main/java/com/example/yoga/navigationdrawer1/FragC.java
@@ -0,0 +1,28 @@
+package com.example.yoga.navigationdrawer1;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yoga on 8/23/2017.
+ */
+
+public class FragC extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_c,container,false);
+    }
+
+    @Override
+    public void onActivityCreated( Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getActivity().setTitle("Menu 3");
+    }
+}
